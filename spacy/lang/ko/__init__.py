0d02393705517a462134342ae13893133e163539
Matthew Honnibal <honnibal+gh@gmail.com>
2018-04-08 23:56:21 +0200

    Add Korean skeleton

---
+++ spacy/lang/ko/__init__.py
@@ -0,0 +1,25 @@
+# encoding: utf8
+from __future__ import unicode_literals, print_function
+import re
+
+from ...language import Language
+from ...attrs import LANG
+from ...tokens import Doc
+from ...tokenizer import Tokenizer
+from ..char_classes import LIST_PUNCT
+
+
+class KoreanDefaults(Language.Defaults):
+    lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters[LANG] = lambda text: 'ko'
+    suffixes = tuple(LIST_PUNCT) + ('\.',)
+
+print(KoreanDefaults.suffixes)
+
+
+class Korean(Language):
+    lang = 'ko'
+    Defaults = KoreanDefaults
+
+
+__all__ = ['Korean']
