b9823903730d5b146e292fa10c307cfaea80a9c1
Matthew Honnibal <honnibal+gh@gmail.com>
2019-02-18 15:06:11 +0100

    Check spaCy builds against murmurhash v1.0.2.dev0

--- setup.py
+++ setup.py
@@ -201,7 +201,7 @@ def setup_package():
             setup_requires=['wheel>=0.32.0,<0.33.0'],
             install_requires=[
                 'numpy>=1.15.0',
-                'murmurhash>=0.28.0,<1.1.0',
+                'murmurhash==1.0.2.dev0',
                 'cymem>=2.0.2,<2.1.0',
                 'preshed>=2.0.1,<2.1.0',
                 'thinc>=6.12.1,<6.13.0',
